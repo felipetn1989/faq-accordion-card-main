cbefbc4ab663726e7c496a783931ce212231a977
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-08 11:13:09 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,22 @@ let arrows = document.querySelectorAll(".question__arrow");
 
 let answers = document.querySelectorAll(".question__answer");
 
-for (let i = 0; i < arrows.length; i++) {
-  arrows[i].addEventListener("click", function () {
+let questions = document.querySelectorAll(".faq__question");
+
+function toggleAnswer(i) {
+  if (answers[i].style.display == "block") {
+    answers[i].style.display = "none";
+    arrows[i].style.transform = "rotate(0deg)";
+    questions[i].style.fontWeight = "normal";
+  } else {
     answers[i].style.display = "block";
     arrows[i].style.transform = "rotate(180deg)";
-    arrows[i].removeEventListener;
-    arrows[i].addEventListener("click", function () {
-      answers[i].style.display = "none";
-      arrows[i].style.transform = "rotate(0deg)";
-    });
+    questions[i].style.fontWeight = "bold";
+  }
+}
+
+for (let i = 0; i < arrows.length; i++) {
+  arrows[i].addEventListener("click", function () {
+    toggleAnswer(i);
   });
 }
